ae505c727ee56629e0a4eb164abe72ea668c1313
sankar <sankarsuresh1997@gmail.com>
2022-11-21 12:24:34 +0530

    first file

---
+++ Number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Number {
+    public static void main(String[] args) {
+        System.out.println("enter your choice");
+        System.out.println("1. largest of three numbers 2.smallest of three numbers 3.prime number 4.even or odd 5.reverse of number");
+        Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+
+       switch(n)
+       {
+
+       }
+    }
+}
