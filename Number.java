38aa4c98e424c114531a3d0b0ae2a8d35f76fc14
sankar <sankarsuresh1997@gmail.com>
2022-11-21 12:35:35 +0530

    largest

--- Number.java
+++ Number.java
@@ -9,6 +9,21 @@ public class Number {
 
        switch(n)
        {
+           case 1:
+               System.out.println("enter three numbers:");
+               int a = sc.nextInt();
+               int b= sc.nextInt();
+               int c= sc.nextInt();
+               if (a>b && a>c)
+               {
+                   System.out.println("a is the largest");
+               }
+               else if (b>a && b>c) {
+                   System.out.println("b is the largest");
+               }
+               else {
+                   System.out.println("c is the largest");
+               }
 
        }
     }
